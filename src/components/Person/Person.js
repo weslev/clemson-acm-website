d92c138c0bbb42bc5eb3b2fa30bc2780ac506f5e
egwebb <xaubreon@gmail.com>
2020-09-23 18:26:54 -0400

    made person cards

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -2,10 +2,16 @@ import React from 'react';
 
 const Person = props => {
     return (
-        <div>
-            <h2>{props.name}</h2>
-            <h3>{props.interests}</h3>
-            <p>Put Image Here</p>
+        <div class="flex justify-center justify-self-center flex-col bg-blue-500 px-4 py-4 m-2">
+            <div class="flex-1">
+                <div class="h-full w-full bg-blue-400 px-4 py-4 m-2">
+                    <p class="text-center">Put Image Here</p>
+                </div>
+                <div class="h-full w-full bg-blue-400 px-4 py-4 m-2">
+                    <h1 class="text-center font-bold text-xl">{props.name}</h1>
+                    <h3 class="text-center">{props.interests}</h3>
+                </div>
+            </div>
         </div>
     );
 }
